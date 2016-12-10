733a585fdb602771525192b21ca4761521a9c3fd
Glycel Banate <glbanate@rogomi.com>
2016-12-10 12:29:18 +0800

    Merge branch 'master' of C:\Users\GLYCHING\Desktop\uhac\Hackathon with conflicts.

--- app/src/main/java/com/example/randolph/hackathon/MainActivity.java
+++ app/src/main/java/com/example/randolph/hackathon/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
         String z = "new Commit";
         String ab = "Practice Repository";
         String test = "rand";
-        String gly = "siopao XD";
+        String push = "Gly Siopao";
     }
 }
