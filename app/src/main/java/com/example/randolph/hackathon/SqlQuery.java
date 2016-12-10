80e9ecc6cf81d0005ad0e47d94326fb3d0827671
pogpog03 <johnpaul.calibo0906@gmail.com>
2016-12-10 17:53:23 +0800

    New query

--- app/src/main/java/com/example/randolph/hackathon/SqlQuery.java
+++ app/src/main/java/com/example/randolph/hackathon/SqlQuery.java
@@ -25,14 +25,15 @@ public class SqlQuery {
     private static final String _sEmail = "Email";
     private static final String _sCardNm = "Card_number";
     private static final String _sPin = "Pin_code";
-
+    private static final String _sBday = "Birthday";
+    private static final String _sContact = "Contact_number";
     public static void createTable(Activity activity){
         try {
             if(activity != null){
                 db = activity.openOrCreateDatabase("db_user", Context.MODE_PRIVATE, null);
                 db.execSQL("CREATE TABLE IF NOT EXISTS" + TABLE_NAME + " ("+_sUid+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         " "+ _sUname+" VARCHAR(30) , "+ _sUpass+" VARCHAR(30), "+ _sFname+" VARCHAR(30), "+ _sMname +" VARCHAR(30)," +
-                        " "+ _sLname+ " VARCHAR(30), "+ _sEmail +" VARCHAR(30), "+_sCardNm+" VARCHAR(50), "+ _sPin+ " INTEGER(5)  );");
+                        " "+ _sLname+ " VARCHAR(30),"+ _sBday+" DATE,"+_sContact+" INTEGER, "+ _sEmail +" VARCHAR(30), "+_sCardNm+" VARCHAR(50), "+ _sPin+ " INTEGER(5)  );");
 
             }
 
@@ -42,14 +43,26 @@ public class SqlQuery {
     }
 
     // _s = String _i = Integer _d = Double --- Variable Standard
-    public void createAccount(String sUname, String sUpass, String sFname, String sMname, String sLname, String sEmail, String sCarnNm,  String sPin){
+    public void createAccount(String sUname, String sUpass, String sFname, String sMname, String sLname,String sBday, String sContact,String sEmail, String sCarnNm,  String sPin){
         try{
             //("+_sUname+", "+_sUpass+" , "+_sFname+", "+_sMname+", "+_sLname+" , "+_sEmail+" ," +"+_sCardNm+", "+_sPin+")
-            db.execSQL("INSERT INTO  "+ TABLE_NAME+"  VALUES (1, '"+sUname+"','"+sUpass+"','"+sFname+"', '"+sMname+"', '"+sLname+"', '"+sEmail+"' , '"+sCarnNm+"', "+sPin+");");
+            db.execSQL("INSERT INTO  "+ TABLE_NAME+"  VALUES (1, '"+sUname+"','"+sUpass+"','"+sFname+"', '"+sMname+"', '"+sLname+"', '"+sBday+"' , "+sContact+", '"+sEmail+"' , '"+sCarnNm+"', "+sPin+");");
 
         }catch (Exception ex){
             ex.printStackTrace();
         }
+
+    }
+
+    public void ediAccount(String sUname, String sUpass, String sFname, String sMname, String sLname,String sBday, String sContact,String sEmail, String sCarnNm,  String sPin)
+    {
+        try {
+            db.execSQL("UPDATE "+TABLE_NAME+ " SET ('"+_sUname+"', '"+_sUpass+"' , '"+_sFname+"', '"+_sMname+"', '"+_sLname+"' , '"+_sBday+"', "+_sContact+",'"+_sEmail+"' ,'"+_sCardNm+"', "+_sPin+") " +
+                    "= ('"+sUname+"','"+sUpass+"','"+sFname+"', '"+sMname+"', '"+sLname+"', '"+sBday+"' , "+sContact+", '"+sEmail+"' , '"+sCarnNm+"', "+sPin+") WHERE "+_sCardNm+"   = '"+sCarnNm+"' ");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
