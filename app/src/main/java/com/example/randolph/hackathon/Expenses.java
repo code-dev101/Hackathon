40029a110cee009e2be3d422ad6b64dbe07f0b71
Conan <ecpasco08@gmail.com>
2016-12-10 19:14:42 +0800

    expense

--- app/src/main/java/com/example/randolph/hackathon/Expenses.java
+++ app/src/main/java/com/example/randolph/hackathon/Expenses.java
@@ -14,6 +14,7 @@ public class Expenses {
     public void initializedCompute(String ctg, int _percent){
         HashMap <String, Integer> hm = new HashMap<>();
         hm.put("food",20);
+
     }
     public void compute(){
         total = (double)(salary - (salary * _percentage));
